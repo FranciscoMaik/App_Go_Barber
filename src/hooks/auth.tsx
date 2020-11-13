4374180969de5990398ac6be33d319977cc6c774
FranciscoMaik <chiconunes3081@gmail.com>
2020-11-12 21:54:56 -0300

    Adding signOut and targeting routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -19,6 +19,7 @@ interface SignInCredentials {
 
 interface AuthContextState {
   user: object;
+  loading: boolean;
   singIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -27,6 +28,7 @@ const AuthContext = createContext<AuthContextState>({} as AuthContextState);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -38,6 +40,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
 
     loadStorageData();
@@ -63,7 +67,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, singIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, singIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
